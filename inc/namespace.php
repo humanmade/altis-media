619009c11c5dd1f2366ea1cecfc27c68a17f40ec
Joe Hoyle <joehoyle@gmail.com>
2019-03-19 12:13:29 +0700

    Pull closures into named functions

--- inc/namespace.php
+++ inc/namespace.php
@@ -2,8 +2,8 @@
 
 namespace HM\Platform\Media;
 
-use HM\Platform;
 use Aws\Rekognition\RekognitionClient;
+use HM\Platform;
 
 /**
  * Bootstrap function to set up the module.
@@ -13,37 +13,58 @@ use Aws\Rekognition\RekognitionClient;
  * @return void
  */
 function bootstrap() {
-	add_action( 'plugins_loaded', function () {
-		$config = Platform\get_config()['modules']['media'];
-		$vendor_dir = Platform\ROOT_DIR . '/vendor';
-		if ( $config['tachyon'] ) {
-			require_once $vendor_dir . '/humanmade/tachyon-plugin/tachyon.php';
-		}
+	add_action( 'plugins_loaded', __NAMESPACE__ . '\\load_plugins', 1 );
+}
 
-		if ( $config['smart-media'] ) {
-			require_once $vendor_dir . '/humanmade/smart-media/plugin.php';
-			remove_filter( 'intermediate_image_sizes_advanced', 'HM\\Media\\Cropper\\prevent_thumbnail_generation' );
-		}
+function load_plugins() {
+	$config = Platform\get_config()['modules']['media'];
+	$vendor_dir = Platform\ROOT_DIR . '/vendor';
 
-		if ( $config['gaussholder'] ) {
-			if ( ! empty( $config['gaussholder']['image-sizes'] ) ) {
-				add_filter( 'gaussholder.image_sizes', function ( $sizes ) use ( $config ) {
-					$sizes = array_merge( $sizes, $config['gaussholder']['image-sizes'] );
-					return $sizes;
-				} );
-			}
-			require_once $vendor_dir . '/humanmade/gaussholder/gaussholder.php';
-		}
+	if ( $config['tachyon'] ) {
+		require_once $vendor_dir . '/humanmade/tachyon-plugin/tachyon.php';
+	}
 
-		if ( $config['rekognition'] ) {
-			/**
-			 * We override the rekognition client to one from our AWS SDK, which
-			 * will correctly pick up the authentication, and global middlewares.
-			 */
-			add_filter( 'hm.aws.rekognition.client', function ( $client, array $params ) : RekognitionClient {
-				return Platform\get_aws_sdk()->createRekognition( $params );
-			}, 10, 2 );
-			require_once $vendor_dir . '/humanmade/aws-rekognition/plugin.php';
+	if ( $config['smart-media'] ) {
+		require_once $vendor_dir . '/humanmade/smart-media/plugin.php';
+		remove_filter( 'intermediate_image_sizes_advanced', 'HM\\Media\\Cropper\\prevent_thumbnail_generation' );
+	}
+
+	if ( $config['gaussholder'] ) {
+		if ( ! empty( $config['gaussholder']['image-sizes'] ) ) {
+			add_filter( 'gaussholder.image_sizes', __NAMESPACE__ . '\\set_gaussholder_image_sizes' );
 		}
-	}, 1 );
+		require_once $vendor_dir . '/humanmade/gaussholder/gaussholder.php';
+	}
+
+	if ( $config['rekognition'] ) {
+		/**
+		 * We override the rekognition client to one from our AWS SDK, which
+		 * will correctly pick up the authentication, and global middlewares.
+		 */
+		add_filter( 'hm.aws.rekognition.client', __NAMESPACE__ . '\\override_aws_rekognition_aws_client', 10, 2 );
+		require_once $vendor_dir . '/humanmade/aws-rekognition/plugin.php';
+	}
+}
+
+/**
+ * Set the image sizes that Gaussholder is applied to.
+ *
+ * @param array $sizes
+ * @return array
+ */
+function set_gaussholder_image_sizes( array $sizes ) : array {
+	$config = Platform\get_config()['modules']['media'];
+	$sizes = array_merge( $sizes, $config['gaussholder']['image-sizes'] );
+	return $sizes;
+}
+
+/**
+ * Override the AWS Rekognition Client with the one from Platform.
+ *
+ * @param null|RekognitionClient $client
+ * @param array $params
+ * @return RekognitionClient
+ */
+function override_aws_rekognition_aws_client( $client, array $params ) : RekognitionClient {
+	return Platform\get_aws_sdk()->createRekognition( $params );
 }
