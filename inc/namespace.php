2f3ae12c66153083cfa47403f3dbd776f5d8e2d3
Joe Hoyle <joehoyle@gmail.com>
2019-03-29 14:42:36 +0700

    Add rekognition keywords to search
    This came up from https://github.com/humanmade/platform-enhanced-search/pull/4

--- inc/namespace.php
+++ inc/namespace.php
@@ -3,6 +3,7 @@
 namespace HM\Platform\Media;
 
 use Aws\Rekognition\RekognitionClient;
+use function HM\AWS_Rekognition\get_attachment_labels;
 use HM\Platform;
 
 /**
@@ -45,6 +46,8 @@ function load_plugins() {
 		 * will correctly pick up the authentication, and global middlewares.
 		 */
 		add_filter( 'hm.aws.rekognition.client', __NAMESPACE__ . '\\override_aws_rekognition_aws_client', 10, 2 );
+		add_filter( 'ep_post_sync_args_post_prepare_meta', __NAMESPACE__ . '\\add_rekognition_keywords_to_search_index', 10, 2 );
+		add_filter( 'ep_search_fields', __NAMESPACE__ . '\\add_rekognition_keywords_to_search_fields' );
 		require_once $vendor_dir . '/humanmade/aws-rekognition/plugin.php';
 	}
 }
@@ -70,4 +73,45 @@ function set_gaussholder_image_sizes( array $sizes ) : array {
  */
 function override_aws_rekognition_aws_client( $client, array $params ) : RekognitionClient {
 	return Platform\get_aws_sdk()->createRekognition( $params );
+}
+
+/**
+ * Add rekognition keywords to the search index
+ *
+ * @param array $post_data
+ * @param integer $post_id
+ * @return array
+ */
+function add_rekognition_keywords_to_search_index( array $post_data, int $post_id ) : array {
+
+	// This filter is called for all post types, so only add data to our "attachemnts" post type.
+	if ( $post_data['post_type'] !== 'attachment' ) {
+		return $post_data;
+	}
+
+	$post_data['alt'] = get_post_meta( $post_id, '_wp_attachment_image_alt', true );
+
+	$labels = get_attachment_labels( $post_id );
+	$labels = array_filter( $labels, function ( array $label ) : bool {
+		return $label['Confidence'] > 50;
+	} );
+	$labels = array_map( function ( array $label ) : string {
+		return $label['Name'];
+	}, $labels );
+	$post_data['rekognition_labels'] = $labels;
+
+	return $post_data;
+}
+
+/**
+ * Add attachment field for search
+ *
+ * @param $search_fields
+ * @return array
+ */
+function add_rekognition_keywords_to_search_fields( array $search_fields ) : array {
+	$search_fields[] = 'alt';
+	$search_fields[] = 'rekognition_labels';
+
+	return $search_fields;
 }
