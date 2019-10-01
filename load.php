afdd8d10a061e0c2f416f630310acad5535f4612
Robert O'Rourke <robert@o-rourke.org>
2019-10-01 14:52:57 +0100

    Support new autoload method

--- load.php
+++ load.php
@@ -4,13 +4,6 @@ namespace Altis\Media; // @codingStandardsIgnoreLine
 
 use function Altis\register_module;
 
-require_once __DIR__ . '/inc/namespace.php';
-
-// Don't self-initialize if this is not an Altis execution.
-if ( ! function_exists( 'add_action' ) ) {
-	return;
-}
-
 add_action( 'altis.modules.init', function () {
 	$default_settings = [
 		'enabled' => true,
