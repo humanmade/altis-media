77c1fabcc3fbc18e40c16e301a20c732fa0e2f15
dashaluna <dashaluna@gmail.com>
2020-02-13 14:38:07 +0000

    Remove AWS_Rekognition filter for performance

--- inc/namespace.php
+++ inc/namespace.php
@@ -17,6 +17,9 @@ use function HM\AWS_Rekognition\get_attachment_labels;
  */
 function bootstrap() {
 	add_action( 'plugins_loaded', __NAMESPACE__ . '\\load_plugins', 1 );
+
+	// Remove AWS_Rekognition filter for performance purposes, as ElasticSearch is used instead.
+	remove_filter( 'posts_clauses', 'HM\\AWS_Rekognition\\filter_query_attachment_keywords' );
 }
 
 /**
