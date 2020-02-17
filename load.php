ceed4e3e12611590cb894f28080b8355132453f5
Robert O'Rourke <robert@o-rourke.org>
2020-02-17 12:34:16 +0000

    split out register callback before function guard

--- load.php
+++ load.php
@@ -6,12 +6,7 @@ use function Altis\register_module;
 
 require_once __DIR__ . '/inc/namespace.php';
 
-// Don't self-initialize if this is not an Altis execution.
-if ( ! function_exists( 'add_action' ) ) {
-	return;
-}
-
-add_action( 'altis.modules.init', function () {
+function register() {
 	$default_settings = [
 		'enabled' => true,
 		'tachyon' => true,
@@ -26,4 +21,11 @@ add_action( 'altis.modules.init', function () {
 		],
 	];
 	register_module( 'media', __DIR__, 'Media', $default_settings, __NAMESPACE__ . '\\bootstrap' );
-} );
+}
+
+// Don't self-initialize if this is not an Altis execution.
+if ( ! function_exists( 'add_action' ) ) {
+	return;
+}
+
+add_action( 'altis.modules.init', __NAMESPACE__ . '\\register' );
