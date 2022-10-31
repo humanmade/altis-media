b414718ea9cded27cf0c9a0ae05689276ec63b4a
robindevitt <robin@humanmade.com>
2022-10-31 14:56:02 +0000

    Add the scripts needed inline.

--- assets/scripts.js
+++
@@ -1,7 +0,0 @@
-// Event listener for when the event altisBlockContentChanged occurs.
-window.addEventListener( 'altisBlockContentChanged', function () {
-	// Ensure images are loaded with GaussHolder if available.
-	if ( window.GaussHolder ) {
-		window.GaussHolder();
-	}
-} );
